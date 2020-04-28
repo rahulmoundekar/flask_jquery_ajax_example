d97a15f43ac397c882db1775466866122d2c7c5d
rahul <rahulmoundekar44@gmail.com>
2020-04-28 15:31:16 +0530

    flask_jquery_ajax_example

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@
 class Config:
     DEBUG = True
     # db
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/djangoapp'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/flaskapp'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
